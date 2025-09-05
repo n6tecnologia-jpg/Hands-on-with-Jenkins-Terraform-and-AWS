aaf22ca5e29d25f0c791fb3f13db482c5f7d1f64
n6tecnologia-jpg <n6tecnologia@gmail.com>
2025-09-05 13:53:58 -0300

    Update DeployReactApp.groovy

--- jobs/DeployReactApp.groovy
+++ jobs/DeployReactApp.groovy
@@ -33,7 +33,6 @@ pipelineJob("Destroy-React-App"){
         daysToKeep(5)
         numToKeep(20)
     }
-    concurrentBuild(allowConcurrentBuild = false)
     parameters {
       stringParam("UNIQUE_ANIMAL_IDENTIFIER", defaultValue = "Elefante", description = "Your unique animal identifier for this playground!")
     }
